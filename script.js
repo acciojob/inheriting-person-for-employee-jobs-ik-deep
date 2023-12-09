3385d34e1145fd519239de02df10aec4987275d6
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-12-09 09:38:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,3 +23,11 @@ Employee.prototype.constructor = Employee;
 Employee.prototype.jobGreet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
+
+// Example usage:
+const person1 = new Person('Alice', 25);
+person1.greet(); // Output: Hello, my name is Alice, I am 30 years old.
+
+const employee1 = new Employee('Bob', 30, 'Manager');
+employee1.greet(); // Output: Hello, my name is Bob, I am 25 years old.
+employee1.jobGreet(); // Output: Hello, my name is Bob, I am 25 years old, and my job title is Developer.
